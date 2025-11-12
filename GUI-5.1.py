c1082108dc6244e95b1b3636eeb25330668e9c87
jxndvs <jxn.dvs13@gmail.com>
2025-11-12 10:22:06 -0600

    Refactor stack display and element handling
    Updated display logic to ensure both text areas are enabled and cleared properly. Adjusted handling of elements in the stack to improve functionality.

--- GUI-5.1.py
+++ GUI-5.1.py
@@ -18,23 +18,28 @@ class Stack:
         self.display()
     def display(self):
         Display1.config(state=NORMAL)
-        Display2.config(state=DISABLED)
+        Display2.config(state=NORMAL)
         list1 = []
         list2 = []
-        if len(self.element) < 21:
+        size = len(self.element)
+        if size < 20:
             for element in self.element:
                 list1.append(element)
-            length = 20-len(self.element)
-            for x in range (0, length):
+            length1 = 20-size
+            for x in range (0, length1):
                 list1.append(" ")
         else:
-            for x in range (0,21):
+            for x in range (0,20):
                 list1.append(self.element[x])
-            for x in range (len(self.element)-21):
-                list2.append(self.element[x+21])
+            for y in range (0,size-20):
+                list2.append(self.element[y+20])
+            length2 = 40 - len(self.element)
+            for x in range(0, length2):
+                list2.append(" ")
         list1.reverse()
         list2.reverse()
         Display1.delete(1.0, "end-1c")
+        Display2.delete(1.0, "end-1c")
         for e in list1:
             Display1.insert(tk.INSERT, f"\n{e}")
         for e in list2:
